c24c440838874d89dc01998e0aa28480f63bbcc1
interger2bit <8826286@gmail.com>
2025-01-31 23:29:07 +0800

    break changes: migrate to blink_cmp, refactor lsp-config, remove some languages lsp.

---
+++ nvim/.config/nvim/lua/plugins/blink_cmp.lua
@@ -0,0 +1,41 @@
+return {
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		{ "L3MON4D3/LuaSnip", version = "v2.*" },
+	},
+	version = "*",
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- 'default' for mappings similar to built-in completion
+		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+		-- See the full "keymap" documentation for information on defining your own keymap.
+		keymap = {
+			preset = "default",
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<S-k>"] = { "scroll_documentation_up", "fallback" },
+			["<S-j>"] = { "scroll_documentation_down", "fallback" },
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+		},
+		appearance = {
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+	},
+	opts_extend = { "sources.default" },
+}
--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -28,18 +28,8 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
 				"lua_ls",
-				"clangd",
-				-- "cmake",
-				"marksman",
 				"bashls",
-				--        "emmet_ls",
-				--        "prismals",
-				--        "pyright",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
@@ -47,16 +37,9 @@ return {
 
 		mason_tool_installer.setup({
 			ensure_installed = {
-				"prettier", -- prettier formatter
+				--				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
-				"cmakelang", -- cmake formatter and linter
-				"markdownlint", -- markdown linter
-				"eslint_d", -- js linter
-				"jsonlint",
 				"shfmt",
-				--        "isort", -- python formatter
-				--        "black", -- python formatter
-				--        "pylint", -- python linter
 			},
 		})
 	end,
