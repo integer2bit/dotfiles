efaab3b2c570a7a6782ed091f5e33effd9bfa4ee
integer2bit <8826286@gmail.com>
2025-03-23 17:10:14 +0800

    feat: fix lsp keymap don't work on buffer

--- nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -20,15 +20,6 @@ return {
 		},
 	},
 	config = function(_, opts)
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-		for server, config in pairs(opts.servers) do
-			-- passing config.capabilities to blink.cmp merges with the capabilities in your
-			-- `opts[server].capabilities, if you've defined it
-			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-			config.on_attach = on_attach
-			lspconfig[server].setup(config)
-		end
 		local on_attach = function(_, bufnr)
 			opts.buffer = bufnr
 			-- set keybinds
@@ -36,56 +27,56 @@ return {
 				"n",
 				"gr",
 				"<cmd>Telescope lsp_references<CR>",
-				{ desc = "Show LSP references", noremap = true, silent = true }
+				{ desc = "Show LSP references", noremap = true, silent = true, buffer = bufnr }
 			) -- show definition, references
 			vim.keymap.set(
 				"n",
 				"gD",
 				vim.lsp.buf.declaration,
-				{ desc = "Go to declaration", noremap = true, silent = true }
+				{ desc = "Go to declaration", noremap = true, silent = true, buffer = bufnr }
 			) -- go to declaration
 			vim.keymap.set(
 				"n",
 				"gd",
 				"<cmd>Telescope lsp_definitions<CR>",
-				{ desc = "Show LSP definitions", noremap = true, silent = true }
+				{ desc = "Show LSP definitions", noremap = true, silent = true, buffer = bufnr }
 			) -- show lsp definitions
 			vim.keymap.set(
 				"n",
 				"gi",
 				"<cmd>Telescope lsp_implementations<CR>",
-				{ desc = "Show LSP implementations", noremap = true, silent = true }
+				{ desc = "Show LSP implementations", noremap = true, silent = true, buffer = bufnr }
 			) -- show lsp implementations
 			vim.keymap.set(
 				"n",
 				"gt",
 				"<cmd>Telescope lsp_type_definitions<CR>",
-				{ desc = "Show LSP type definitions", noremap = true, silent = true }
+				{ desc = "Show LSP type definitions", noremap = true, silent = true, buffer = bufnr }
 			) -- show lsp type definitions
 			vim.keymap.set(
 				{ "n", "v" },
 				"<leader>ca",
 				vim.lsp.buf.code_action,
-				{ desc = "See available code actions", noremap = true, silent = true }
+				{ desc = "See available code actions", noremap = true, silent = true, buffer = bufnr }
 			) -- see available code actions, in visual mode will apply to selection
 			vim.keymap.set(
 				"n",
 				"<leader>rn",
 				vim.lsp.buf.rename,
-				{ desc = "Smart rename", noremap = true, silent = true }
+				{ desc = "Smart rename", noremap = true, silent = true, buffer = bufnr }
 			) -- smart rename
 
 			vim.keymap.set(
 				"n",
 				"<leader>D",
 				"<cmd>Telescope diagnostics bufnr=0<CR>",
-				{ desc = "Show buffer diagnostics", noremap = true, silent = true }
+				{ desc = "Show buffer diagnostics", noremap = true, silent = true, buffer = bufnr }
 			) -- show  diagnostics for file
 			vim.keymap.set(
 				"n",
 				"<leader>d",
 				vim.diagnostic.open_float,
-				{ desc = "Show line diagnostics", noremap = true, silent = true }
+				{ desc = "Show line diagnostics", noremap = true, silent = true, buffer = bufnr }
 			) -- show diagnostics for line
 
 			-- neovim v0.10 default keymap
@@ -96,8 +87,17 @@ return {
 				"n",
 				"<leader>rs",
 				":LspRestart<CR>",
-				{ desc = "Restart LSP", noremap = true, silent = true }
+				{ desc = "Restart LSP", noremap = true, silent = true, buffer = bufnr }
 			) -- mapping to restart lsp if necessary
+		end
+		-- import lspconfig plugin
+		local lspconfig = require("lspconfig")
+		for server, config in pairs(opts.servers) do
+			-- passing config.capabilities to blink.cmp merges with the capabilities in your
+			-- `opts[server].capabilities, if you've defined it
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			config.on_attach = on_attach
+			lspconfig[server].setup(config)
 		end
 		-- default diagnostic settings
 		vim.diagnostic.enable(false)
