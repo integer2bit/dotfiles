6fd1ca0f7fdb36cd8b4a0215a62e1e265c68c8ee
world <8826286@gmail.com>
2025-05-14 20:08:45 +0800

    chore(lsp.lua): change lsp diagnostics settings

--- nvim/.config/nvim/lua/opt/lsp.lua
+++ nvim/.config/nvim/lua/opt/lsp.lua
@@ -1,14 +1,22 @@
 -- keymap
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" }) -- show diagnostics for line
--- default diagnostic settings
-vim.diagnostic.enable(false)
--- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+-- vim.lsp.diagnostic settings
+vim.diagnostic.config({
+	float = {
+		source = true,
+	},
+	virtual_lines = {
+		current_line = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "󰠠",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+	},
+})
 -- Enable LSP servers
 local lsp_configs = {}
 for _, v in ipairs(vim.api.nvim_get_runtime_file("lsp/*", true)) do
