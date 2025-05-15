ccf7af4e4d5d75873f58aea695aa560dc0c7bc7f
world <8826286@gmail.com>
2025-05-15 20:01:22 +0800

    chore(lazydev): change lazydev config

--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -1,11 +1,9 @@
 return {
-	"saghen/blink.cmp",
-	{
-		"folke/lazydev.nvim",
-		opts = {
-			library = {
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
+	"folke/lazydev.nvim",
+	ft = { "lua" },
+	opts = {
+		library = {
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 		},
 	},
 }
