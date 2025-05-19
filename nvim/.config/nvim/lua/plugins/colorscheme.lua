ded7f4621f9bbe2feee0bf1a9e81612d2419e092
world <8826286@gmail.com>
2025-05-19 16:52:08 +0800

    chore: change colorscheme to nvim dark(ghostty,nvim), tmux minimal status plugin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-	"olimorris/onedarkpro.nvim",
-	priority = 1000, -- Ensure it loads first
-	opts = function(_, opts)
-		vim.cmd.colorscheme("onedark")
-	end,
+	-- "olimorris/onedarkpro.nvim",
+	-- priority = 1000, -- Ensure it loads first
+	-- opts = function(_, opts)
+	-- 	vim.cmd.colorscheme("onedark")
+	-- end,
 }
