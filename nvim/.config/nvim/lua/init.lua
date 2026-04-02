d9044a9d7307f0df253253d06950df2b9cb574a8
world <8826286@gmail.com>
2026-04-02 20:38:16 +0800

    feat(nvim): remove lualine plugin, add custom status line

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -3,3 +3,4 @@ vim.g.maplocalleader = ","
 require("opt.keymap")
 require("opt.options")
 require("opt.lsp")
+require("opt.statusline")
---
+++ nvim/.config/nvim/lua/opt/statusline.lua
@@ -0,0 +1,90 @@
+-- Refernce:https://vieitesss.github.io/posts/Neovim-custom-status-line/#full-implementation
+Statusline = {}
+vim.opt.showcmd = true
+vim.opt.showcmdloc = "statusline"
+
+vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
+	callback = function()
+		vim.cmd("redrawstatus")
+	end,
+})
+
+function Statusline.get_diagnostics()
+	if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then
+		return ""
+	end
+
+	local count = {
+		errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }),
+		warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN }),
+	}
+
+	local result = {}
+	if count.errors > 0 then
+		table.insert(result, "" .. count.errors)
+	end
+	if count.warnings > 0 then
+		table.insert(result, "" .. count.warnings)
+	end
+
+	return #result > 0 and (table.concat(result, " ") .. " ") or ""
+end
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+	callback = function()
+		vim.cmd("redrawstatus")
+	end,
+})
+
+function Statusline.active()
+	local keys = " %S "
+	-- macro recording
+	local reg = vim.fn.reg_recording()
+	local macro = reg ~= "" and ("Macro @" .. reg .. " ") or ""
+	-- filepath
+	local filepath = "[%f] %m%r "
+	-- align
+	local align_right = "%="
+	-- filetype
+	local filetype = "%Y"
+	-- lsp diagnostics
+	local diagnostics = Statusline.get_diagnostics()
+	-- percentage and the current line and column of the cursor
+	local percentage = "%p%%"
+	local position = "%l:%c"
+
+	local left_section = keys .. macro .. filepath .. diagnostics
+	local right_section = filetype .. " │ " .. percentage .. " │ " .. position .. " "
+	return left_section .. align_right .. right_section
+end
+
+function Statusline.inactive()
+	return "[%f] %m"
+end
+
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "#14161b", fg = "#d8dee9" })
+
+local group = vim.api.nvim_create_augroup("Statusline", { clear = true })
+
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+	group = group,
+	desc = "Update statusline on diagnostic change",
+	callback = function()
+		vim.cmd("redrawstatus")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	group = group,
+	desc = "Activate statusline on focus",
+	callback = function()
+		vim.opt_local.statusline = "%!v:lua.Statusline.active()"
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+	group = group,
+	desc = "Deactivate statusline when unfocused",
+	callback = function()
+		vim.opt_local.statusline = "%!v:lua.Statusline.inactive()"
+	end,
+})
